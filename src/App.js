f42e17624417e5040faa4a88299e785585a602bc
ilcomarian <ilcomarian@gmail.com>
2018-11-08 10:16:54 -0500

    porfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import "./App.css";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
+import Routes from "./components/routs";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navbar inverse collapseOnSelect className="heder-color">
+          <Navbar.Header>
+            <Navbar.Brand>
+              <NavLink
+                to="/"
+                exact
+                activeStyle={{ color: "grey" }}
+                style={{ color: "black" }}
+              >
+                <h4> MyPortfolio</h4>
+              </NavLink>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1}>
+                <NavLink
+                  to="/projects"
+                  className="link"
+                  activeClassName="activ"
+                >
+                  <h4>Projects</h4>
+                </NavLink>
+              </NavItem>
+              <NavItem
+                eventKey={2}
+                href="/marian.pdf"
+                target="_blank"
+                className="link"
+              >
+                {/* <a href="/marian.pdf" target="_blank" className="link">
+                  <h4>Resume</h4>
+                </a> */}{" "}
+                {/* <NavLink
+                  topathname="/marian.pdf"
+                  target="_blank"
+                  className="link"
+                > */}
+                <h4>Resume</h4>
+                {/* </NavLink> */}
+              </NavItem>
+              <NavItem eventKey={3}>
+                <NavLink
+                  to="/contacts"
+                  className="link"
+                  activeClassName="activ"
+                >
+                  <h4>Contacts</h4>
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes />
       </div>
     );
   }
